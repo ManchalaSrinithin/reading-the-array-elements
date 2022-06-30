a14bf53cfeef703533be3f30aaaeebe2202e8735
ManchalaSrinithin <108468976+ManchalaSrinithin@users.noreply.github.com>
2022-06-30 18:31:57 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a[100],i,n;
+	printf("Enter any value:");
+	scanf("%d",&n);
+	printf("Enter elements in array");
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+}
